9d1bb8c261c7f2bf8362457fc564290e22157df6
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2023-03-31 10:21:37 +0900

    インプット おそうじ

--- Engine/INPUT/PadInput.h
+++ Engine/INPUT/PadInput.h
@@ -132,57 +132,7 @@ public:
 
 };
 
-// direct input の残骸
-/*
-#include <dinput.h>
-#define DIRECTINPUT_VERSION 0x0800 // DirectInputのバージョン指定
 
-#pragma comment(lib, "dinput8.lib")
-#pragma comment(lib, "dxguid.lib")
-
-class Pad_D_Input
-{
-public: // メンバ関数
-	// 初期化
-	void Initialize(WinApp* winApp);
-
-	// 更新
-	void Update();
-
-	/// <summary>
-	/// キーの押下をチェック
-	/// </summary>
-	/// <param name = "keyNumber">キー番号(DIK_0 等)</param>
-	/// <returns>押されているか</returns>
-	bool PushKey(BYTE keyNumber);
-
-	/// <summary>
-	/// キーのトリガーをチェック
-	/// </summary>
-	/// </param name="keyNumber">キー番号( DIK_0 等)</param>
-	/// <reutrns>トリガーか</params>
-	bool TriggerKey(BYTE keyNumber);
-
-	/// <summary>
-	/// キーのトリガーをチェック
-	/// </summary>
-	/// </param name="keyNumber">キー番号( DIK_0 等)</param>
-	/// <reutrns>離されたか</params>
-	bool ReleaseKey(BYTE keyNumber);
-
-private: // メンバ変数
-	// キーボードのデバイス
-	ComPtr<IDirectInputDevice8> D_Pad_;
-	// DirectInputのインスタンス
-	ComPtr<IDirectInput8> directInput;
-	// 全キーの状態
-	BYTE key[256] = {};
-	// 前回の全キーの状態
-	BYTE keyPre[256] = {};
-	//windwsAPI
-	WinApp* winApp_ = nullptr;
-
-};*/
 
 
 
