3a5be754ec2f4c6134cac41abc89b2bfecbcad1a
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2023-06-16 15:19:45 +0900

    数学編集

--- Engine/MATH/Vector2.cpp
+++ Engine/MATH/Vector2.cpp
@@ -1,10 +1,8 @@
 #include "Vector2.h"
-#include <cmath>
+#include<cmath>  //sprt
 
-Vector2::Vector2()
+Vector2::Vector2() :x(0), y(0)
 {
-	x = 0;
-	y = 0;
 }
 
 Vector2::Vector2(float x, float y) {
@@ -13,11 +11,10 @@ Vector2::Vector2(float x, float y) {
 }
 
 float Vector2::length()const {
-
-	return (sqrt(x) * sqrt(x)) + (sqrt(y) * sqrt(y));
+	return std::sqrt(x * x + y * y);
 }
 
-Vector2& Vector2::nomalize() {
+Vector2& Vector2::normalize() {
 	float len = length();
 	if (len != 0) {
 		return *this /= len;
@@ -25,68 +22,69 @@ Vector2& Vector2::nomalize() {
 	return *this;
 }
 
-float Vector2::dot(const Vector2& v) const {
-	return (this->x * v.x) + (this->y * v.y);
+float Vector2::dot(const Vector2& v) const
+{
+	return x * v.x + y * v.y;
 }
 
-float Vector2::cross(const Vector2& v)const {
-	return (this->x * v.y) - (this->y * v.x);
+float Vector2::cross(const Vector2& v) const
+{
+	return x * v.y - y * v.x;
 }
 
-Vector2 Vector2::operator+()const {
-	return *this;
-}
-
-Vector2 Vector2::operator-()const {
-	return Vector2(-(*this));         ////
-}
-
-Vector2& Vector2::operator+=(const Vector2& v) {
+Vector2& Vector2::operator+=(const Vector2& v)
+{
 	this->x += v.x;
 	this->y += v.y;
 	return *this;
 }
 
-Vector2& Vector2::operator-=(const Vector2& v) {
+Vector2& Vector2::operator-=(const Vector2& v)
+{
 	this->x -= v.x;
 	this->y -= v.y;
 	return *this;
 }
 
-Vector2& Vector2::operator*=(float s) {
+Vector2& Vector2::operator*=(float s)
+{
 	this->x *= s;
 	this->y *= s;
 	return *this;
 }
 
-Vector2& Vector2::operator/=(float s) {
-	this->x /= s;
-	this->y /= s;
+Vector2& Vector2::operator/=(float s)
+{
+	x /= s;
+	y /= s;
 	return *this;
 }
 
-//Vector2D　クラスに属さない関数群
-//二項演算子
-const Vector2 operator+(const Vector2& v1, const Vector2& v2) {
+const Vector2 operator+(const Vector2& v1, const Vector2& v2)
+{
 	Vector2 temp(v1);
-	return temp + v2;
+	return temp += v2;
 }
 
-const Vector2 operator-(const Vector2& v1, const Vector2& v2) {
+const Vector2 operator-(const Vector2& v1, const Vector2& v2)
+{
 	Vector2 temp(v1);
-	return temp - v2;
+	return temp -= v2;
 }
 
-const Vector2 operator*(const Vector2& v, float s) {
+const Vector2 operator*(const Vector2& v, float s)
+{
 	Vector2 temp(v);
 	return temp * s;
 }
 
-const Vector2 operator*(float s, const Vector2& v) {
-	return s * v;
+const Vector2 operator*(float s, const Vector2& v)
+{
+	return v * s;
 }
 
-const Vector2 operator/(const Vector2& v, float s) {
+const Vector2 operator/(const Vector2& v, float s)
+{
 	Vector2 temp(v);
 	return temp / s;
 }
--- Engine/MATH/Vector3.cpp
+++ Engine/MATH/Vector3.cpp
@@ -1,14 +1,18 @@
 #include "Vector3.h"
+
 #include <cmath>
 
 Vector3::Vector3()
-	:x(0), y(0), z(0)
 {
+	x = 0;
+	y = 0;
+	z = 0;
 }
 
-Vector3::Vector3(float x, float y, float z)
-	: x(x), y(y), z(z)
-{
+Vector3::Vector3(float x, float y, float z) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
 }
 
 float Vector3::length()const {
@@ -18,21 +22,17 @@ float Vector3::length()const {
 Vector3& Vector3::nomalize() {
 	float len = length();
 	if (len != 0) {
-		return *this /= len;
+		return *this /= length();
 	}
 	return *this;
 }
 
-float Vector3::dot(const Vector3& v)const {
-	return(x * v.x) + (y * v.y) + (z * v.z);
+float Vector3::dot(const Vector3& v) const {
+	return (this->x * v.x) + (this->y * v.y) + (this->z * v.z);
 }
 
-Vector3 Vector3::cross(const Vector3& v)const {
-	Vector3 ans;
-
-	ans = { (y * v.z) - (z * v.y),(z * v.x) - (x * v.z), (x * v.y) - (y * v.x) };
-
-	return ans;
+Vector3 Vector3::cross(const Vector3& v) const {
+	return Vector3((this->y * v.z) - (this->z * v.y), (this->z * v.x) - (this->x * v.z), (this->x * v.y) - (this->y * v.x));
 }
 
 const Vector3 Vector3::lerp(const Vector3& start, const Vector3& end, const float t) {
@@ -41,86 +41,82 @@ const Vector3 Vector3::lerp(const Vector3& start, const Vector3& end, const floa
 	return start * (1.0f - t) + end * t;
 }
 
-Vector3 Vector3::Vector3Zero() {
-	Vector3 a = { 0, 0, 0 };
-	return a;
-}
-
-bool Vector3::Vector3Equal(Vector3 v1, Vector3 v2) {
-	if (v1.x == v2.x && v1.y == v2.y && v1.z == v2.z) {
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-bool Vector3::Vector3IsInfinite() {
-	if (isfinite(this->x) && isfinite(this->y) && isfinite(this->z)) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
 Vector3 Vector3::operator+()const {
 	return *this;
 }
 
 Vector3 Vector3::operator-()const {
-	return Vector3(-x, -y, -z);
+
+	return Vector3(-x, -y, -z);         ////
 }
 
 Vector3& Vector3::operator+=(const Vector3& v) {
-	x += v.x;
-	y += v.y;
-	z += v.z;
-	return*this;
+	this->x += v.x;
+	this->y += v.y;
+	this->z += v.z;
+	return *this;
 }
 
 Vector3& Vector3::operator-=(const Vector3& v) {
-	x -= v.x;
-	y -= v.y;
-	z -= v.z;
-	return*this;
+	this->x -= v.x;
+	this->y -= v.y;
+	this->z -= v.z;
+	return *this;
 }
 
 Vector3& Vector3::operator*=(float s) {
-	x *= s;
-	y *= s;
-	z *= s;
-	return*this;
+	this->x *= s;
+	this->y *= s;
+	this->z *= s;
+	return *this;
 }
 
 Vector3& Vector3::operator/=(float s) {
-	x /= s;
-	y /= s;
-	z /= s;
-	return*this;
+	this->x /= s;
+	this->y /= s;
+	this->z /= s;
+	return *this;
 }
 
-//Vector3?N???X????????????Q
-//2?????Z?q
+//Vector2D　クラスに属さない関数群
+//二項演算子
 const Vector3 operator+(const Vector3& v1, const Vector3& v2) {
 	Vector3 temp(v1);
-	return temp += v2;
+	temp.x += v2.x;
+	temp.y += v2.y;
+	temp.z += v2.z;
+	return temp;
 }
+
 const Vector3 operator-(const Vector3& v1, const Vector3& v2) {
-	Vector3 tenp(v1);
-	return tenp -= v2;
+	Vector3 temp(v1);
+	temp.x -= v2.x;
+	temp.y -= v2.y;
+	temp.z -= v2.z;
+	return temp;
 }
 
 const Vector3 operator*(const Vector3& v, float s) {
 	Vector3 temp(v);
-	return temp *= s;
+	temp.x *= s;
+	temp.y *= s;
+	temp.z *= s;
+	return temp;
 }
 
 const Vector3 operator*(float s, const Vector3& v) {
-	return v * s;
+
+	Vector3 result;
+	result.x = v.x * s;
+	result.y = v.y * s;
+	result.z = v.z * s;
+	return result;
 }
 
 const Vector3 operator/(const Vector3& v, float s) {
 	Vector3 temp(v);
-	return v / s;
+	temp.x /= s;
+	temp.y /= s;
+	temp.z /= s;
+	return temp;
 }
