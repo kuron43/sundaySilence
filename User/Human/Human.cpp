8223520d9e4a27ee5014c7470140877aac1eda03
FujitaKentaro <k021g1261@g.neec.ac.jp>
2023-06-30 15:47:35 +0900

    ワーニング消し

--- User/Human/Human.cpp
+++ User/Human/Human.cpp
@@ -16,10 +16,14 @@ void Human::Initialize() {
 /// シーンの更新を行う
 void Human::Update(Input* input,bool isTitle) {
 	assert(input);
+	if (input) {
+
+	}
 	_isTitle = isTitle;
 }
 
 /// シーンの描画を行う
 void Human::Draw(DirectXCommon* dxCommon) {
 	assert(dxCommon);
+	dxCommon->GetDevice();
 }
